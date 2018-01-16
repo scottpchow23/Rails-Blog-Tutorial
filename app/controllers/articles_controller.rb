0981c9c6fc215b00563f009a8c2875ac6485011b
Scott Chow <tehchowster@gmail.com>
2018-01-15 16:47:08 -0800

    Added the ability to validate length of title and show those errors accordingly.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else 
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
